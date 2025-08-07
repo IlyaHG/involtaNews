798862b2901148752730d0fd860f01e4b76f1553
Илья Куратов <kurat.ilya@gmail.com>
2025-08-07 04:42:29 +0300

    feat!: btn-fixes

--- components/news/NewsHeader.vue
+++ components/news/NewsHeader.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import RefreshIcon from '~/static/refresh.svg'
+import RefreshIcon from '../../static/refresh.svg'
 import NewsHeaderSearch from './NewsHeaderSearch.vue'
 
 const emit = defineEmits(['refresh', 'search'])
@@ -14,12 +14,13 @@ watch(query, (val) => {
   emit('search', val)
 })
 </script>
+
 <template>
   <div class="news__header">
     <h1 class="text-2xl font-bold mb-4 news__title">Список новостей</h1>
 
     <button class="news__header--refresh-button" @click="$emit('refresh')">
-      <RefreshIcon />
+      <RefreshIcon class="refresh-icon" />
     </button>
 
     <div class="news__header--search">
@@ -28,16 +29,12 @@ watch(query, (val) => {
   </div>
 </template>
 
-
-
-
 <style scoped>
 .news__header {
   display: flex;
   align-items: center;
   gap: 1rem;
   font-size: var(--font-size-xl);
-
 }
 
 .news__title {
@@ -45,12 +42,23 @@ watch(query, (val) => {
 }
 
 .news__header--refresh-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 32px;
+  height: 32px;
+  padding: 0;
   background-color: #fff;
   border: none;
   border-radius: 50%;
   color: var(--primary-color);
   box-shadow: 0 1px 3px rgba(0, 0, 0, 0.1);
+  cursor: pointer;
+}
 
+.refresh-icon {
+  width: 16px; /* Размер иконки можно регулировать */
+  height: 16px;
 }
 
 .news__header--search {
